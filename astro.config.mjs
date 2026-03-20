bca2a362c74d26cdecca9e6ab7cde5588a0b2eba
srijanghosh05 <srijanghosh03@gmail.com>
2026-03-20 10:24:30 +0530

    Updated engine file

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
+  devToolbar: {
+    enabled: false
+  },
   vite: {
     plugins: [tailwindcss()]
   },
